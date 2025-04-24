d5cd256ec88302e420df1331b261daa22a7b7b4b
immobarok <immobarokali2001@gmail.com>
2025-04-25 00:01:11 +0600

    first-commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../../assets/logo.png'
+function Header() {
+  return (
+    <div>
+      <img src={logo} alt="" />
+    </div>
+  )
+}
+
+export default Header
